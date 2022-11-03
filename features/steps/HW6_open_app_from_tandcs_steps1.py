1176725dbe21c7d76324117d9580549b75981abd
mr-paulab <mr.paulab@gmail.com>
2022-11-02 22:32:03 -0400

    HW6 Q1 steps file

--- features/steps/HW6_open_app_from_tandcs_steps1.py
+++ features/steps/HW6_open_app_from_tandcs_steps1.py
@@ -3,37 +3,43 @@ from behave import given, when, then
 from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 
-DOG_IMG = (By.CSS_SELECTOR, "img[alt='Dogs of Amazon']")
+#PG_HDR = (By.CSS_SELECTOR, "div.help-content h1")
 
-@given('Store original window')
+@given('Open cou page')
+def open_cou_pg(context):
+    context.driver.get("https://www.amazon.com/gp/help/customer/display.html?nodeId=508088&ref_=footer_cou")
+
+
+@when('Store original windows')
 def original_window(context):
     context.original_window = context.driver.current_window_handle
     print('Original: ', context.original_window)
 
 
-@when('Click on dog image')
-def click_img(context):
-    context.driver.find_element(*DOG_IMG).click()
+@when('Click on Amazon Privacy Notice link')
+# in the body of text
+def click_apn_link(context):
+    element = context.driver.find_element(By.CSS_SELECTOR, "[href='https://www.amazon.com/privacy']").click()
 
 
-@when('Switch to new window')
-def switch_to_new_window(context):
-    context.driver.wait.until(EC.new_window_is_opened)
-    current_windows = context.driver.window_handles
-    print('Current windows: ', current_windows)
-    context.driver.switch_to.window(current_windows[1])
+#@when('Switch to new window')
+#def switch_to_new_window(context):
+#    context.driver.wait.until(EC.new_window_is_opened)
+#    current_windows = context.driver.window_handles
+#    print('Current windows: ', current_windows)
+#    context.driver.switch_to.window(current_windows[1])
 
 
-@then('Verify blog is opened')
-def verify_blog_opened(context):
-    context.driver.wait.until(EC.url_contains('https://www.aboutamazon.com/news/workplace/'))
+@then('Verify Amazon Privacy Notice page is opened')
+def verify_apn_page_opened(context):
+    context.driver.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div.help-content h1'), 'Amazon.com Privacy Notice'))
+    print('Amazon Privacy Notice page verified')
 
-
-@then('Close blog')
+@then('Close new window and switch back to original')
 def close_blog(context):
     context.driver.close()
-
-
-@then('Return to original window')
-def return_to_original(context):
+    print('Amazon Privacy Notice page closed')
     context.driver.switch_to.window(context.original_window)
+    print('Returned to COU page')
+
+
